cfb167c4e0ec48777eb5580bda9561f84bd4d466
cderry1 <conor.derry@kainos.com>
2023-07-06 13:15:25 +0100

    Sales Create done

--- src/main/java/org/kainos/ea/api/SalesEmployeeService.java
+++ src/main/java/org/kainos/ea/api/SalesEmployeeService.java
@@ -1,2 +1,36 @@
-package org.kainos.ea.api;public class SalesEmployeeService {
+package org.kainos.ea.api;
+
+import org.kainos.ea.cli.DeliveryEmployeeRequest;
+import org.kainos.ea.cli.SalesRequest;
+import org.kainos.ea.client.FailedToCreateEmployeeException;
+import org.kainos.ea.client.InvalidEmployeeException;
+import org.kainos.ea.db.DeliveryEmployeeDao;
+import org.kainos.ea.db.SalesEmployeeDao;
+
+import java.sql.SQLException;
+
+public class SalesEmployeeService {
+    private SalesEmployeeDao salesEmployeeDao = new SalesEmployeeDao();
+    public int createSalesEmployee(SalesRequest sale) throws InvalidEmployeeException, FailedToCreateEmployeeException {
+        try{
+          //  String validation = salesEmployeeValidator.validateDeliveryEmployee(employee);
+
+//            if (validation != null){
+  //              throw new InvalidEmployeeException(validation);
+    //        }
+
+            int id = salesEmployeeDao.createSalesEmployee(sale);
+
+            if (id == -1){
+                throw new FailedToCreateEmployeeException();
+            }
+
+            return id;
+
+        } catch (SQLException e){
+            System.err.println(e.getMessage());
+
+            throw new FailedToCreateEmployeeException();
+        }
+    }
 }
--- src/main/java/org/kainos/ea/cli/SalesRequest.java
+++ src/main/java/org/kainos/ea/cli/SalesRequest.java
@@ -1,2 +1,22 @@
-package org.kainos.ea.cli;public class SalesRequest {
+package org.kainos.ea.cli;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class SalesRequest extends EmployeeRequest  {
+    private double comRate;
+
+    public double getComRate() {
+        return comRate;
+    }
+
+    public void setComRate(double comRate) {
+        this.comRate = comRate;
+    }
+
+    @JsonCreator
+    public SalesRequest(@JsonProperty("name") String name, @JsonProperty("salary") double salary, @JsonProperty("bankAccountNo") String bankAccountNo, @JsonProperty("natInsuranceNo") String natInsuranceNo, @JsonProperty("CommisionRate") double comRate) {
+        super(name, salary, bankAccountNo, natInsuranceNo);
+        setComRate(comRate);
+    }
 }
